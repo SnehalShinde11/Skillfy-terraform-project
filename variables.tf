2e2b5b9b1964dce2aa6859941cb48e55eeea9e11
SnehalShinde11 <83006384+SnehalShinde11@users.noreply.github.com>
2026-09-24 23:20:05 +0530

    Change default instance type to 't3.micro'

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.small"
+  default     = "t3.micro"
 }
 
 variable "vpc_cidr" {
